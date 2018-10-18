a43f27a4b87c13c2c9931a79753c5ec925b48e3f
Faizan <mirzafaizanejaz@gmail.com>
2018-10-18 18:59:47 +0500

    changed URL (now in production)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const RecRoutes = require('./api/routes/record');
 const FarmRoutes = require('./api/routes/farms');
 const mongoose = require('mongoose');
-var mongoURI = "mongodb://localhost:27017/IoT";
+var mongoURI = 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/IoT?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
