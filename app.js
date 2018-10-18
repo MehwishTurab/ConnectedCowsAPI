7921deee976047d1ef4532056f40e017660a25a3
Faizan <mirzafaizanejaz@gmail.com>
2018-10-18 18:48:30 +0500

    changed something

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ var mongoURI = "mongodb://localhost:27017/IoT";
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 //setting up mongoDB connection
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI).catch((error) => {
     console.log("Exception \n"+error);
   });
 mongoose.connection.on('error',function(){
-    console.log("Could not connect to DB, exiting now ...");
+    console.log("Could not connect to DB, exiting now. ");
     process.exit();
 });
 mongoose.connection.once('open',function(){
@@ -24,11 +23,9 @@ mongoose.connection.once('open',function(){
 });
 app.use('/record',RecRoutes);
 app.use('/farms',FarmRoutes);
-
 app.use((req,res,next) => {
     const error = new Error('Not found');
     error.status = 404;
 });
-
 
 module.exports = app;
