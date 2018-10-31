eee30dd9363f78d2d0c8dd860a63fc13dff57b32
Faizan <mirzafaizanejaz@gmail.com>
2018-10-31 10:50:45 +0500

    changed mongo URL

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const OwnerRoutes = require('./api/routes/owner');
 const CattleRoutes = require('./api/routes/cattle');
 const RecordRoutes = require('./api/routes/record');
 const mongoose = require('mongoose');
-var mongoURI = 'mongodb://localhost:27017/IoT'; // for prodcution use.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/IoT?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
-//var mongoURI = 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/IoT?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+// var mongoURI = 'mongodb://localhost:27017/IoT'; // for prodcution use.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/IoT?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
+var mongoURI = 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/IoT?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
