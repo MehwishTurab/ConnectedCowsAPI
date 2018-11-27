a10a93f14c95eae32d1be99abeee5a78ea629eb0
Faizan <mirzafaizanejaz@gmail.com>
2018-11-27 22:11:00 +0500

    added message controller and model

---
+++ api/Controller/MessageController.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Message = require('../model/messageSch');
+
+//Function to create farm
+exports.CreateNewMessage = function (req, res) {
+    var message = new Message({
+        message : req.body.message,
+    });
+    farm.save(function (err) {
+        if (err)
+            return res.json(err);
+        else
+            res.send({
+                message: "MSG created Successfully",
+                
+            });
+        console.log("Data entered");
+    });
+}
+
+//Function to Fetch all Farms
+exports.fetchallMessages = function (req, res) {
+    Message.find()
+    .select('message')
+    .exec()
+    .then(doc => {
+        const response = {
+            count: doc.length,
+            info: doc.map(docs => {
+                return {
+                    message:docs.message
+                }
+            })
+        }
+            res.status(200).json(response);  
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+};
+
