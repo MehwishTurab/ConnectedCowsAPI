e20119c1146b1243b6865f2fe376e791f03d4e02
Faizan <mirzafaizanejaz@gmail.com>
2019-04-15 10:28:44 +0500

    modified cattle schema

--- api/model/cattleSch.js
+++ api/model/cattleSch.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const CattleSchema = mongoose.Schema({
     cattle_id: mongoose.Schema.Types.ObjectId,
     farmid: { type: String, required: true },
-    ownerid: { type: String, required: true }
+    ownerid: { type: String, required: true },
+    cattle_name:{type: String, required:false}
 });
 
 module.exports = mongoose.model('Cattle',CattleSchema);
