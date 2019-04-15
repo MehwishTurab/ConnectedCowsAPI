3ce93325c2cefce1ec579473dc5aa57e188939e3
Faizan <mirzafaizanejaz@gmail.com>
2019-04-15 10:30:49 +0500

    modified messages schema, added owner id

--- api/model/messageSch.js
+++ api/model/messageSch.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
     message: {type: String, required: true},
+    owner_id : {type:String, require: true},
 });
 
 module.exports = mongoose.model('message',messageSchema);
